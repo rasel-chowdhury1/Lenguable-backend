abecbc8c5c86be8335fb42df8277357d930345d1
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2026-06-22 14:34:15 +0600

    Remove dist from tracking

--- dist/app/modules/teacher/teacher.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status["ACTIVE"] = "ACTIVE";
-    Status["WARNING"] = "WARNING";
-})(Status || (exports.Status = Status = {}));
